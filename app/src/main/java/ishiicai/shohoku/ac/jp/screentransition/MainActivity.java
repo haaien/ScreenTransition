097808aca74296d60757f392c9db3dafddc225f6
haien <s18b701@shohoku.ac.jp>
2019-10-17 15:02:33 +0900

    MainAcvitity.javaの追加
    エラーはかれてるけど気にして

--- app/src/main/java/ishiicai/shohoku/ac/jp/screentransition/MainActivity.java
+++ app/src/main/java/ishiicai/shohoku/ac/jp/screentransition/MainActivity.java
@@ -1,9 +1,65 @@
 package ishiicai.shohoku.ac.jp.screentransition;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+
+public class MainActivity extends Activity{
+    int count = 1;
+    @override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+        //ボタンを押したときにイベントを取得できるようにする
+        Button button = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @override
+            public void onClick(View v){
+                if(count == 1){
+                    //変更したいレイアウトを取得する
+                    LinearLayout layout = (ListenerLayout) findViewById((R.id.listenerlayout1);
+                    //ﾚｲｱｳﾄをビューをすべて削除する
+                    layout.removeAllViews();
+                    //レイアウトをR.layout.sampleに変更する
+                    getLayoutInflater().inflate(R.layout.view2, layout);
+                    count=2;
+                }else if (count == 2){
+                    LinearLayout Layout = (LinearLayout) findViewById(R.id.linearlayout2);
+                    layout.removeAllViews();
+                    getLayoutInflater().Inflater().inflate(R.layout.view1, layout);
+                    count=1;
+            }
+        }
+    });
+    }
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu){
+        //infate the menu;  this adds items to the action bar if it is present.
+        return true;
+    }
+
+    @Override
+    public  boolean onOptionsitemSelected(MenuItem item){
+        //Handble action bar clicks here. The action bar will
+        //automatically handle clisks on the Home/Up button, so ling
+        //as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimlifiableIfStatement
+        if(id == R.id.action_settings){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+}
+/*
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,3 +68,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+*/
